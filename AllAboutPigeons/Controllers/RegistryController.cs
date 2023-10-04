b4d0596c0954f3273fefb0f3c3e2ab09dfce51e4
Brian Bird <birdb@lanecc.edu>
2023-10-04 11:19:54 -0700

    Added some views

---
+++ AllAboutPigeons/Controllers/RegistryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AllAboutPigeons.Controllers
+{
+    public class RegistryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Info()
+        {
+            return View();
+        }
+
+    }
+}
