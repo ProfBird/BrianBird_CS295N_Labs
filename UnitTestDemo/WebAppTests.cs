44d352a4f3dabd073f4ba6234b0ac94920a66498
Brian Bird <birdb@lanecc.edu>
2026-01-27 13:51:35 -0800

    Added a controller method test.

---
+++ UnitTestDemo/WebAppTests.cs
@@ -0,0 +1,23 @@
+﻿using AllAboutPigeons.Controllers;
+using AllAboutPigeons.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTestDemo
+{
+    public class WebAppTests
+    {
+        [Fact]
+        public void ForumPostTest()
+        {
+            // Arrange
+            var registryController = new RegistryController();
+            var model = new Message();
+            // Act
+            var viewResult = (ViewResult)registryController.ForumPost(model);
+            var message = viewResult.Model as Message;
+            // Assert
+            Assert.NotNull(message);
+            Assert.Equal(DateOnly.FromDateTime(DateTime.Now), message!.Date);
+        }
+    }
+}
